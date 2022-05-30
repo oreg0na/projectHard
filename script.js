0ca2d37ce53e02c8af18557d648c245c8bce09f7
Slava Pogosov <62066211+oreg0na@users.noreply.github.com>
2022-05-30 21:43:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-let num = 266219;
-let newArr = num.toString().split('');
-console.log(newArr);
-let res = newArr.reduce((acc, rec) => acc * rec);
-console.log(res);
-console.log((res ** 3).toString().slice(0, 2))
+'use strict';
+
+const question = confirm('Знаете ли вы английский?')
+let lang = question === true ? 'en' : 'ru';
+const daysArr = [
+    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+];
+
+if (lang === 'en') {
+    alert(daysArr[0]);
+} else {
+    alert(daysArr[1]);
+}
+
+//Вывод через switch
+switch (lang) {
+    case 'en':
+        alert(daysArr[0]);
+        break;
+    case 'ru':
+        alert(daysArr[1]);
+        break;
+    default:
+        console.log('Что то сломалось');
+        break;
+}
+
+let map = new Map();
+
+map.set('en', daysArr[0])
+    .set('ru', daysArr[1]);
+
+alert(map.get(lang));
+
+const namePerson = prompt('Введите свое имя', 'Артем').toLowerCase();
+
+namePerson === 'артем' || namePerson === 'артём' ? console.log('директор')
+    : namePerson === 'александр' ? console.log('преподаватель') : console.log('студент');
