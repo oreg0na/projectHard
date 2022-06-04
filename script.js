84610845b48f0c7ca2f8da7bbfc9b392b8f082f2
Slava Pogosov <62066211+oreg0na@users.noreply.github.com>
2022-06-04 20:38:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let num = 266219;
-let newArr = num.toString().split('');
-console.log(newArr);
-let res = newArr.reduce((acc, rec) => acc * rec);
-console.log(res);
-console.log((res ** 3).toString().slice(0, 2))
+'use strict';
+
+let arr = [];
+let letters = prompt('Введите больше или меньше 30 символов', 'Надо дописать еще немного');
+
+function myfunc(str) {
+    if (typeof str !== 'string') {
+        return 'Не строка';
+    } else {
+        let strCut = str.trim();
+        if (strCut.length > 30) {
+            strCut = strCut.substring(0, 30) + '...';
+            return strCut;
+        } else {
+            return 'Попробуйте ввести больше символов';
+        }
+    };
+};
+
+
+alert(myfunc(letters));
+console.log(myfunc(arr));
