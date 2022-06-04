c3b6a2c5b71ee67e4081f55ceb22a6522e067dec
Slava Pogosov <62066211+oreg0na@users.noreply.github.com>
2022-06-04 20:54:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,80 @@
 'use strict';
 
-let arr = ['222', '22', '2', '4444', '524', '64', '72'];
+const hiddenNumber = function (myNumber, min, max) {
 
-arr.forEach((item) => {
-    if (item.startsWith('2') || item.startsWith('4')) {
-        console.log(item);
+    let startGame;
+    let gameOver;
+    let attempts = 10;
+
+    startGame = confirm("Угадай число от 1 до 100?");
+
+    if (startGame) {
+
+        takeNumber(myNumber, attempts);
+
+    } else {
+        gameOver = alert("Игра окончена");
     }
-});
-
-nextPrime:
-for (let i = 2; i < 100; i++) {
-
-    for (let j = 2; j < i; j++) {
-        if (i % j == 0) continue nextPrime;
-    }
-
-    console.log(i + ' Делители этого числа: 1 и ' + i);
 };
+
+const takeNumber = function (unknowNum, attempts) {
+    let enterNumber;
+
+    console.log(attempts);
+
+    enterNumber = prompt('Введите число от 1 до 100');
+
+    if (enterNumber === null) {
+        return alert('Игра окончена');
+    }
+
+    while (enterNumber.trim() == '' || !isFinite(enterNumber)) {
+        alert('Введите число!');
+        enterNumber = prompt('Введите число от 1 до 100');
+    }
+
+    enterNumber = Number(enterNumber);
+
+    attempts--;
+
+    if (enterNumber < unknowNum) {
+        numLessAlert(attempts);
+    }
+
+    if (enterNumber > unknowNum) {
+        numMoreAlert(attempts);
+    }
+
+    if (enterNumber === unknowNum) {
+        return alert('Поздравляю, Вы угадали!!!');
+    }
+
+
+    if (attempts === 0) {
+        let choose;
+
+        choose = confirm('Попытки закончились, хотите сыграть еще?');
+
+        if (choose) {
+            attempts = 10;
+        }
+    }
+
+    takeNumber(unknowNum, attempts);
+};
+
+const numLessAlert = function (attempt) {
+
+    alert('Загаданное число больше, осталось ' + attempt + ' попыток');
+};
+
+const numMoreAlert = function (attempt) {
+
+    alert('Загаданное число меньше, осталось ' + attempt + ' попыток');
+};
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+hiddenNumber(getRandomInt(1, 100)) > 10;
